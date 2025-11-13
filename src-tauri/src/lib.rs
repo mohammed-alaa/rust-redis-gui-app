9277653de62fbea17524accbb77fc44d4aef61da
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-11-14 01:46:45 +0200

    Add auto-update support with Tauri Updater
    Integrated tauri-plugin-updater for desktop platforms, added updater configuration and public key, and implemented update logic in Rust. Updated dependencies and configuration files to enable automatic updates and release artifact generation.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,19 +1,27 @@
 mod commands;
 mod state;
+mod utils;
 
 use commands::{connect_to_server, get_keys};
 use state::AppState;
 use std::sync::Mutex;
 use tauri::{Builder, Manager};
+use utils::update;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     Builder::default()
         .setup(|app| {
             app.manage(Mutex::new(AppState::new()));
+            let handle = app.handle().clone();
+
+            tauri::async_runtime::spawn(async move {
+                update(handle).await.unwrap();
+            });
             Ok(())
         })
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![connect_to_server, get_keys])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
