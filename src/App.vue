2321598b70628425a4d0f908ca1efd07e40c42d9
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-11-16 18:52:57 +0200

    Refactor routing and move server UI to Home view
    Moved server-related UI from App.vue to a new Home view (src/views/Home/index.vue). Updated routes.ts to define routes for Home and AddServer views, improving code organization and separation of concerns.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
 import { check } from "@tauri-apps/plugin-updater";
-import { storeToRefs } from "pinia";
-import { useStore } from "./Store";
-import { AddServer } from "@modules/AddServer";
 
 async function checkFor() {
 	// Disable update check in development mode
@@ -18,32 +15,9 @@ async function checkFor() {
 	}
 }
 
-const store = useStore();
-const { isConnected, isLoadingKeys, keys, filter } = storeToRefs(store);
 checkFor();
 </script>
 
 <template>
 	<router-view />
-	<template v-if="isConnected">
-		<template v-if="isLoadingKeys">
-			<p>Loading keys...</p>
-		</template>
-		<template v-else>
-			<form @submit="store.getKeys">
-				<input v-model="filter" placeholder="Filter keys..." />
-			</form>
-
-			<div class="overflow-y-scroll m-4 p-4 border">
-				<ul>
-					<template v-for="key in keys" :key="`keys-list-${key}`">
-						<li>{{ key }}</li>
-					</template>
-				</ul>
-			</div>
-		</template>
-	</template>
-	<template v-else>
-		<AddServer @server-added="store.onConnect" />
-	</template>
 </template>
--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,14 @@
 import { type RouteRecordRaw } from "vue-router";
 
-export default [] as RouteRecordRaw[];
+export default [
+	{
+		name: "home",
+		path: "/",
+		component: () => import("@views/Home/index.vue"),
+	},
+	{
+		name: "add-server",
+		path: "/add-server",
+		component: () => import("@views/AddServer/index.vue"),
+	},
+] as RouteRecordRaw[];
