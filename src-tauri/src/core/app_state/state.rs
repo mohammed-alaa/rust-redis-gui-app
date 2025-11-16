2ef5b1c26e8ff65f414b140bef289c69be3c2102
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-11-16 22:18:32 +0200

    Refactor AppState and move to core module
    AppState has been refactored to support both Redis and database connections, and moved from the state module to the new core/app_state module. The old state module and its implementation have been removed to improve code organization and extensibility.

---
+++ src-tauri/src/core/app_state/state.rs
@@ -0,0 +1,26 @@
+use crate::core::Database;
+use redis::Client;
+
+#[derive(Debug, Default)]
+pub struct AppState {
+    redis_client: Option<Client>,
+    db_connection: Option<Database>,
+}
+
+impl AppState {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn set_redis_client(&mut self, client: Option<Client>) {
+        self.redis_client = client;
+    }
+
+    pub fn set_db_connection(&mut self, db_connection: Option<Database>) {
+        self.db_connection = db_connection;
+    }
+
+    pub fn get_redis_client(&self) -> Option<&Client> {
+        self.redis_client.as_ref()
+    }
+}
