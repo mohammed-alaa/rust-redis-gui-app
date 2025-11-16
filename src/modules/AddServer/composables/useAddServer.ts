89382fc6a8ff63b7cb1a3a9bf359e033f2d6e1ce
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-11-16 03:51:44 +0200

    Feat(WIP): add add server form schema

--- src/modules/AddServer/composables/useAddServer.ts
+++ src/modules/AddServer/composables/useAddServer.ts
@@ -1,11 +1,22 @@
 import { ref } from "vue";
 import { invoke } from "@tauri-apps/api/core";
 import { COMMANDS, DEFAULT_SERVER } from "@constants";
+import { toTypedSchema } from "@vee-validate/zod";
+import { z } from "zod";
 
 export function useAddServer(onSuccess: Function) {
 	const isLoading = ref(false);
 	const error = ref<string | null>(null);
 	const form = ref({ ...DEFAULT_SERVER });
+
+	const formSchema = toTypedSchema(
+		z.object({
+			name: z.string().min(2).max(50),
+			host: z.string().min(2).max(50),
+			// username: z.string().min(2).max(50),
+			port: z.number().min(1).max(65535),
+		}),
+	);
 
 	async function onSubmit() {
 		error.value = null;
@@ -25,6 +36,7 @@ export function useAddServer(onSuccess: Function) {
 		form,
 		isLoading,
 		error,
+		formSchema,
 
 		onSubmit,
 	};
