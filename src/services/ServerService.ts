f1f4a53e30cd99ff568b75761ec67277ed61da43
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-11-17 14:27:10 +0200

    Add ServerService for server management
    Introduces ServerService with methods to add and retrieve servers using Tauri API invocations. This service centralizes server-related operations for easier maintenance and reuse.

---
+++ src/services/ServerService.ts
@@ -0,0 +1,24 @@
+import { COMMANDS } from "@constants";
+import { invoke } from "@tauri-apps/api/core";
+
+export class ServerService {
+	constructor() {}
+
+	async addServer(values: TServerFormFields) {
+		try {
+			const server = await invoke<TServer>(COMMANDS.ADD_SERVER, values);
+			return Promise.resolve<TServer>(server);
+		} catch (error) {
+			return Promise.reject(error);
+		}
+	}
+
+	async getServers(): Promise<TServer[]> {
+		try {
+			const servers = await invoke<TServer[]>(COMMANDS.GET_SERVERS);
+			return Promise.resolve(servers);
+		} catch (error) {
+			return Promise.reject(error);
+		}
+	}
+}
