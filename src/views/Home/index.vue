1b9f5b7b02263d86514ed8fe7bc212692fe750ac
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-11-22 20:52:13 +0200

    Refactor: update home page to use table components

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -2,6 +2,16 @@
 import { storeToRefs } from "pinia";
 import { useServerStore } from "@stores/useServerStore";
 import { Button } from "@components/ui/button";
+import {
+	Table,
+	TableBody,
+	TableEmpty,
+	TableCell,
+	TableHeader,
+	TableRow,
+	TableHead,
+	TableFooter,
+} from "@components/ui/table";
 
 const serverStore = useServerStore();
 const { servers, isLoading } = storeToRefs(serverStore);
@@ -10,28 +20,48 @@ serverStore.getServers();
 </script>
 
 <template>
-	<div class="w-screen h-screen grid justify-center">
-		<h1>Welcome to Redis GUI</h1>
-
-		<template v-if="isLoading">
-			<p>Loading servers...</p>
-		</template>
-		<template v-else>
-			<div class="grid grid-cols-1 gap-2">
+	<div class="w-screen h-screen p-4">
+		<Table>
+			<TableHeader>
+				<TableRow>
+					<TableHead> Name </TableHead>
+					<TableHead>Address</TableHead>
+					<TableHead>Actions</TableHead>
+				</TableRow>
+			</TableHeader>
+			<TableBody>
 				<template
 					v-for="server in servers"
 					:key="`servers-server-${server.id}`"
 				>
-					<div class="grid grid-cols-3 items-center justify-betweeny">
-						<h2>{{ server.name }}</h2>
-						<p>{{ server.address }}:{{ server.port }}</p>
-					</div>
+					<TableRow>
+						<TableCell class="font-medium">
+							{{ server.name }}
+						</TableCell>
+						<TableCell>
+							{{ server.address }}:{{ server.port }}
+						</TableCell>
+						<TableCell> </TableCell>
+					</TableRow>
 				</template>
-			</div>
-		</template>
-
-		<RouterLink :to="{ name: 'add-server' }">
-			<Button>Add Server</Button>
-		</RouterLink>
+			</TableBody>
+			<TableEmpty v-if="isLoading || !servers.length">
+				<template v-if="isLoading">
+					<p>Loading servers...</p>
+				</template>
+				<template v-else>
+					<p>No servers found.</p>
+				</template>
+			</TableEmpty>
+			<TableFooter>
+				<TableRow>
+					<TableCell colspan="100%" class="text-center">
+						<RouterLink :to="{ name: 'add-server' }">
+							<Button>Add Server</Button>
+						</RouterLink>
+					</TableCell>
+				</TableRow>
+			</TableFooter>
+		</Table>
 	</div>
 </template>
