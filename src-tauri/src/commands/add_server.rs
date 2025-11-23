e4d00854bfc418b21ac85c269c43494bdf73f8ca
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-11-23 20:36:03 +0200

    Refactor server commands to use AppError and add tests
    Updated add_server and get_servers commands to return AppError instead of String for error handling. Extracted core logic into private functions for easier testing and added comprehensive unit tests for both commands, improving reliability and maintainability.

--- src-tauri/src/commands/add_server.rs
+++ src-tauri/src/commands/add_server.rs
@@ -1,10 +1,29 @@
 use crate::{
-    core::AppState,
+    core::{AppError, AppState},
     models::{Model, Server},
     services::test_connection,
 };
 use std::sync::Mutex;
 use tauri::State;
+
+fn _add_server(
+    state: &Mutex<AppState>,
+    name: String,
+    address: String,
+    port: u16,
+) -> Result<Server, AppError> {
+    let state = state.lock().unwrap();
+    let db_connection = state.get_db_connection();
+
+    if db_connection.is_none() {
+        return Err(AppError::DbNotReady);
+    }
+
+    let server = Server::from_payload(name, address, port);
+    test_connection(&server)?;
+
+    server.create(db_connection.unwrap())
+}
 
 #[tauri::command]
 pub fn add_server(
@@ -12,18 +31,76 @@ pub fn add_server(
     name: String,
     address: String,
     port: u16,
-) -> Result<Server, String> {
-    let state = state.lock().unwrap();
-    let db_connection = state.get_db_connection();
+) -> Result<Server, AppError> {
+    _add_server(state.inner(), name, address, port)
+}
 
-    if db_connection.is_none() {
-        return Err("Database connection is not available".into());
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use crate::{
+        core::{AppState, Database},
+        models::Server,
+        tests::run_redis_container,
+    };
+    use std::sync::Mutex;
+
+    #[tokio::test]
+    async fn test_add_working_server() {
+        let (host, port, container) = run_redis_container(6379).await;
+
+        let app_state = Mutex::new({
+            let mut s = AppState::new();
+            s.set_db_connection(Some(Database::new_in_memory().unwrap()));
+            s
+        });
+
+        let test_server = Server::from_payload("Local Redis".into(), host, port);
+
+        let result = _add_server(
+            &app_state,
+            test_server.name.clone(),
+            test_server.address.clone(),
+            test_server.port,
+        );
+
+        assert!(result.is_ok());
+        let server = result.unwrap();
+        assert_eq!(server.name, test_server.name);
+        assert_eq!(server.address, test_server.address);
+        assert_eq!(server.port, test_server.port);
+        assert_ne!(server, test_server);
+
+        drop(container);
     }
 
-    let server = Server::from_payload(name, address, port);
-    test_connection(&server)?;
+    #[test]
+    fn test_add_wrong_server() {
+        let app_state = Mutex::new({
+            let mut s = AppState::new();
+            s.set_db_connection(Some(Database::new_in_memory().unwrap()));
+            s
+        });
 
-    server
-        .create(db_connection.unwrap())
-        .map_err(|err| format!("Failed to create server: {}", err))
+        let result = _add_server(
+            &app_state,
+            "Bad Server".into(),
+            "256.256.256.256".into(),
+            6379,
+        );
+
+        assert!(result.is_err());
+        assert_eq!(result.err().unwrap(), AppError::RedisFailed);
+    }
+
+    #[test]
+    fn test_add_server_no_db_connection() {
+        let app_state = Mutex::new(AppState::new());
+
+        let server = _add_server(&app_state, "Test Server".into(), "localhost".into(), 6379);
+
+        assert!(server.is_err());
+        assert_eq!(server.err().unwrap(), AppError::DbNotReady);
+    }
 }
--- src-tauri/src/commands/get_servers.rs
+++ src-tauri/src/commands/get_servers.rs
@@ -1,20 +1,87 @@
-use std::sync::Mutex;
-
-use tauri::State;
-
 use crate::{
-    core::AppState,
+    core::{AppError, AppState},
     models::{Model, Server},
 };
+use std::sync::Mutex;
+use tauri::State;
 
-#[tauri::command]
-pub fn get_servers(state: State<'_, Mutex<AppState>>) -> Result<Vec<Server>, String> {
+fn _get_servers(state: &Mutex<AppState>) -> Result<Vec<Server>, AppError> {
     let state = state.lock().unwrap();
     let db_connection = state.get_db_connection();
 
     if db_connection.is_none() {
-        return Err("Database connection is not available".into());
+        return Err(AppError::DbNotReady);
     }
 
-    Server::get(db_connection.unwrap()).map_err(|err| format!("Failed to get servers: {}", err))
+    Server::get(db_connection.unwrap())
+}
+
+#[tauri::command]
+pub fn get_servers(state: State<'_, Mutex<AppState>>) -> Result<Vec<Server>, AppError> {
+    _get_servers(state.inner())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        core::{AppState, Database},
+        models::Server,
+    };
+    use std::sync::Mutex;
+    // use tauri::test::{assert_ipc_response, mock_builder, mock_context, noop_assets, MockRuntime};
+
+    // fn create_app() -> tauri::App<MockRuntime> {
+    //     let app_state = Mutex::new({
+    //         let mut s = AppState::new();
+    //         s.set_db_connection(Some(Database::new_in_memory().unwrap()));
+    //         s
+    //     });
+
+    //     mock_builder()
+    //         .manage(app_state)
+    //         .invoke_handler(tauri::generate_handler![get_servers])
+    //         .build(mock_context(noop_assets()))
+    //         .expect("failed to build app")
+    // }
+
+    #[test]
+    fn test_get_servers_empty_db() {
+        let app_state = Mutex::new({
+            let mut s = AppState::new();
+            s.set_db_connection(Some(Database::new_in_memory().unwrap()));
+            s
+        });
+
+        let servers = _get_servers(&app_state);
+
+        assert!(servers.is_ok());
+        assert_eq!(servers.unwrap().len(), 0);
+    }
+
+    #[test]
+    fn test_get_servers_with_data() {
+        let app_state = Mutex::new({
+            let mut s = AppState::new();
+            s.set_db_connection(Some(Database::new_in_memory().unwrap()));
+            s
+        });
+
+        let server = Server::new().create(app_state.lock().unwrap().get_db_connection().unwrap());
+        let result = _get_servers(&app_state);
+
+        assert!(result.is_ok());
+        let servers = result.unwrap();
+        assert_eq!(servers.len(), 1);
+        assert_eq!(servers[0], server.unwrap())
+    }
+
+    #[test]
+    fn test_get_servers_no_db() {
+        let app_state = Mutex::new(AppState::new());
+        let servers = _get_servers(&app_state);
+
+        assert!(servers.is_err());
+        assert_eq!(servers.err().unwrap(), AppError::DbNotReady);
+    }
 }
