c8d8e94af39a1785122c7223034bd530581f6a34
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-11-24 04:52:55 +0200

    Add unit tests for Server view component
    Introduces a new test suite for the Server view using Vitest and Vue Test Utils. The tests verify correct rendering and setup with mocked dependencies and router configuration.

---
+++ src/tests/views/Server/index.test.ts
@@ -0,0 +1,69 @@
+import { createPinia } from "pinia";
+import { createRouter, createWebHashHistory } from "vue-router";
+import { mount } from "@vue/test-utils";
+import { describe, it, vi, beforeEach, afterEach, expect } from "vitest";
+import { mockIPC, clearMocks } from "@tauri-apps/api/mocks";
+import { toast } from "vue-sonner";
+import Server from "@views/Server/index.vue";
+import { useServerFactory } from "@test-utils/useServerFactory";
+import { APP_ERROR_CODES, COMMANDS } from "@constants";
+import { useServerStore } from "@stores/useServerStore";
+import { ServerService } from "@services/ServerService";
+
+vi.spyOn(toast, "error").mockImplementation((msg, __data) => msg as string);
+
+describe("Home View", () => {
+	let servers: TServer[];
+	let componentWrapper: ReturnType<typeof mount>;
+
+	beforeEach(() => {
+		servers = Array.from(
+			{ length: 3 },
+			() => useServerFactory().validServer().server,
+		);
+
+		componentWrapper = mount(Server, {
+			global: {
+				plugins: [
+					createPinia(),
+					createRouter({
+						history: createWebHashHistory(),
+						routes: [
+							{
+								name: "home",
+								path: "/",
+								component: async () =>
+									import("@views/Home/index.vue"),
+							},
+							{
+								name: "server",
+								path: "/server",
+								component: Server,
+							},
+							{
+								name: "add-server",
+								path: "/add-server",
+								component: async () =>
+									import("@views/Server/index.vue"),
+							},
+						],
+					}),
+				],
+			},
+		});
+	});
+
+	afterEach(() => {
+		if (componentWrapper) {
+			componentWrapper.unmount();
+		}
+
+		servers = [];
+		clearMocks();
+		vi.clearAllMocks();
+	});
+
+	it("should render correctly", async () => {
+		expect(componentWrapper.exists()).toBe(true);
+	});
+});
