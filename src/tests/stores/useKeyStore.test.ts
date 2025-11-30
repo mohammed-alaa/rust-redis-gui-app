f29a785503fb77d119be1229c3bc720b861f4065
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-11-30 08:54:26 +0200

    Feat: add key store

---
+++ src/tests/stores/useKeyStore.test.ts
@@ -0,0 +1,121 @@
+import { beforeEach, expect, describe, it, afterEach, vi } from "vitest";
+import { setActivePinia, createPinia } from "pinia";
+import { useKeyStore } from "@stores/useKeyStore";
+import { clearMocks, mockIPC } from "@tauri-apps/api/mocks";
+import { APP_ERROR_CODES, COMMANDS } from "@constants";
+import { KeyService } from "@services/KeyService";
+
+describe("useKeyStore", () => {
+	beforeEach(() => {
+		setActivePinia(createPinia());
+	});
+
+	afterEach(() => {
+		clearMocks();
+		vi.clearAllMocks();
+	});
+
+	describe("retrieve keys", () => {
+		it("fetches keys correctly", async () => {
+			const mockKeys: TKey[] = [
+				{ key: "key1", key_type: "string", ttl: 3600 },
+				{ key: "key2", key_type: "hash", ttl: -1 },
+			];
+
+			mockIPC((cmd) => {
+				if (cmd === COMMANDS.RETRIEVE_KEYS) {
+					return mockKeys;
+				}
+			});
+
+			const keyStore = useKeyStore();
+
+			const loadingKeys = keyStore.retrieveKeys({
+				pattern: "*",
+				limit: 100,
+			});
+
+			expect(keyStore.isLoading).toBe(true);
+			await vi.waitFor(async () => loadingKeys);
+
+			expect(keyStore.isLoading).toBe(false);
+			await expect(loadingKeys).resolves.toEqual(mockKeys);
+			expect(keyStore.keys).toEqual(mockKeys);
+		});
+
+		it("handles errors when fetching keys", async () => {
+			mockIPC((cmd) => {
+				if (cmd === COMMANDS.RETRIEVE_KEYS) {
+					throw {
+						code: APP_ERROR_CODES.REDIS_FAILED,
+					};
+				}
+			});
+
+			const keyStore = useKeyStore();
+
+			const loadingKeys = keyStore.retrieveKeys({
+				pattern: "*",
+				limit: 100,
+			});
+
+			expect(keyStore.isLoading).toBe(true);
+			await vi.waitFor(async () => loadingKeys.catch(() => {}));
+
+			expect(keyStore.isLoading).toBe(false);
+			await expect(loadingKeys).rejects.toEqual({
+				code: APP_ERROR_CODES.REDIS_FAILED,
+			});
+			expect(keyStore.keys).toEqual([]);
+		});
+	});
+
+	describe("retrieve key", () => {
+		it("fetches a key correctly", async () => {
+			const mockKey: TKey = {
+				key: "key1",
+				key_type: "string",
+				ttl: 3600,
+			};
+
+			mockIPC((cmd, args) => {
+				if (
+					cmd === COMMANDS.RETRIEVE_KEY &&
+					(args!.key as TKey["key"]) === "key1"
+				) {
+					return mockKey;
+				}
+			});
+
+			const keyStore = useKeyStore();
+
+			const loadingKey = keyStore.retrieveKey("key1");
+
+			await vi.waitFor(async () => loadingKey);
+
+			await expect(loadingKey).resolves.toEqual(mockKey);
+			expect(keyStore.currentKey).toEqual(mockKey);
+		});
+
+		it("handles errors when fetching a key", async () => {
+			mockIPC((cmd) => {
+				if (cmd === COMMANDS.RETRIEVE_KEY) {
+					throw {
+						code: APP_ERROR_CODES.REDIS_FAILED,
+					};
+				}
+			});
+
+			const keyStore = useKeyStore();
+
+			const loadingKey = keyStore.retrieveKey("key1");
+
+			await vi.waitFor(async () => loadingKey.catch(() => {}));
+
+			await expect(loadingKey).rejects.toEqual({
+				code: APP_ERROR_CODES.REDIS_FAILED,
+			});
+			expect(keyStore.currentKey).toBeNull();
+		});
+	});
+});
