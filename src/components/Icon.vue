cbd55011ef3c313c5345dd8db2175b7f71b30ed8
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-11-30 09:31:25 +0200

    Feat: add icon component based Iconify's

---
+++ src/components/Icon.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Icon as Base, type IconProps } from "@iconify/vue";
+
+const props =
+	defineProps<Omit<IconProps, "ssr" | "id" | "title" | "mode" | "inline">>();
+</script>
+
+<template>
+	<Base v-bind="props" />
+</template>
