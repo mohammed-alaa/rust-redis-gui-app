3bccdf93638cd2a9b37a82bd1d6de3db7e5bc041
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-12-01 09:10:33 +0200

    Feat: move keys table & filter form to separate components

---
+++ src/views/Server/components/FilterForm.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import {
+	FormField,
+	FormItem,
+	FormControl,
+	FormMessage,
+} from "@components/ui/form";
+import { Input } from "@components/ui/input";
+
+defineEmits<{
+	"submit:filters": [event: Event];
+}>();
+</script>
+
+<template>
+	<form
+		data-testid="filter-keys-form"
+		class="flex flex-col gap-4"
+		@submit.prevent="$emit('submit:filters', $event)"
+	>
+		<FormField bails name="pattern" v-slot="{ componentField }">
+			<FormItem>
+				<FormControl>
+					<Input
+						type="text"
+						placeholder="Filter by pattern (e.g. user:*)"
+						data-testid="filter-keys-form-pattern-field"
+						v-bind="componentField"
+					/>
+				</FormControl>
+				<FormMessage
+					data-testid="filter-keys-form-pattern-field-error"
+				/>
+			</FormItem>
+		</FormField>
+	</form>
+</template>
--- src/views/Server/index.vue
+++ src/views/Server/index.vue
@@ -2,32 +2,20 @@
 import { storeToRefs } from "pinia";
 import { RouterLink } from "vue-router";
 import { useServerStore } from "@stores/useServerStore";
+import { useKeyStore } from "@stores/useKeyStore";
 import { onBeforeUnmount } from "vue";
 import { toast } from "vue-sonner";
 import { useFilterForm } from "./composables/useFilterForm";
 import { Button } from "@components/ui/button";
-import {
-	FormField,
-	FormItem,
-	FormControl,
-	FormMessage,
-} from "@components/ui/form";
-import { Input } from "@components/ui/input";
-import {
-	Table,
-	TableHead,
-	TableBody,
-	TableRow,
-	TableHeader,
-	TableCell,
-} from "@components/ui/table";
-import { useKeyStore } from "@stores/useKeyStore";
+import KeysTable from "./components/KeysTable.vue";
+import FilterForm from "./components/FilterForm.vue";
+import CurrentKeyDetailts from "./components/CurrentKeyDetailts.vue";
 
 const serverStore = useServerStore();
-const keyStore = useKeyStore();
-const { keys } = storeToRefs(keyStore);
-const { form, onSubmit: onFiltersSubmitted } = useFilterForm();
 const { activeServer, isConnected } = storeToRefs(serverStore);
+const keyStore = useKeyStore();
+const { keys, currentKey } = storeToRefs(keyStore);
+const { form, onSubmit: onFiltersSubmitted } = useFilterForm();
 
 onBeforeUnmount(() => {
 	serverStore.closeServer().catch(() => {
@@ -39,6 +27,14 @@ async function onSubmit(event: Event) {
 	try {
 		const values = await onFiltersSubmitted(event);
 		await keyStore.retrieveKeys(values!);
+	} catch (error) {
+		toast.error(`${error}`);
+	}
+}
+
+async function onKeyClick(key: TKey["key"]) {
+	try {
+		await keyStore.retrieveKey(key);
 	} catch (error) {
 		toast.error(`${error}`);
 	}
@@ -65,73 +61,18 @@ keyStore.retrieveKeys(form.values).catch((error) => toast.error(`${error}`));
 				</strong>
 			</p>
 
-			<form
-				data-testid="filter-keys-form"
-				class="flex flex-col gap-4"
-				@submit.prevent="onSubmit"
-			>
-				<FormField bails name="pattern" v-slot="{ componentField }">
-					<FormItem>
-						<FormControl>
-							<Input
-								type="text"
-								placeholder="Filter by pattern (e.g. user:*)"
-								data-testid="filter-keys-form-pattern-field"
-								v-bind="componentField"
-							/>
-						</FormControl>
-						<FormMessage
-							data-testid="filter-keys-form-pattern-field-error"
-						/>
-					</FormItem>
-				</FormField>
-			</form>
-
-			<div
-				class="overflow-auto rounded-md border"
-				style="max-height: 400px"
-			>
-				<Table class="table-fixed">
-					<colgroup>
-						<col width="50%" />
-					</colgroup>
-					<TableHeader sticky class="bg-muted shadow">
-						<TableRow>
-							<TableHead class="text-center"> Key </TableHead>
-							<TableHead class="text-center"> Type </TableHead>
-							<TableHead class="text-center"> TTL </TableHead>
-							<!-- <TableHead class="text-center">
-								Memory Usage
-							</TableHead> -->
-						</TableRow>
-					</TableHeader>
-					<TableBody>
-						<template
-							v-for="key in keys"
-							:key="`keys-key-${key.key}`"
-						>
-							<TableRow>
-								<TableCell>
-									<p
-										class="break-all truncate hover:line-clamp-2"
-										:title="key.key"
-									>
-										{{ key.key }}
-									</p>
-								</TableCell>
-								<TableCell class="text-center">
-									<p>{{ key.key_type }}</p>
-								</TableCell>
-								<TableCell class="text-center">
-									<p>{{ key.ttl }}</p>
-								</TableCell>
-								<!-- <TableCell class="text-center">
-									<p>{{ key.memory_usage }}</p>
-								</TableCell> -->
-							</TableRow>
-						</template>
-					</TableBody>
-				</Table>
+			<div class="grid grid-cols-2 h-full gap-2 flex-wrap max-h-full">
+				<div class="flex flex-col gap-2">
+					<FilterForm @submit:filters="onSubmit" />
+					<KeysTable
+						class="flex-1"
+						:keys="keys"
+						@click:key="onKeyClick"
+					/>
+				</div>
+				<div>
+					<CurrentKeyDetailts v-bind="currentKey" />
+				</div>
 			</div>
 		</div>
 	</template>
