ca9c443f61fb8477efd2aeb0751e3935100eb33d
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-12-01 09:10:46 +0200

    Feat: add current key details component

---
+++ src/views/Server/components/CurrentKeyDetailts.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+withDefaults(
+	defineProps<{
+		details?: TKey | null;
+		content?: TKeyContent | null;
+	}>(),
+	{
+		details: null,
+		content: null,
+	},
+);
+</script>
+
+<template>
+	{{ details }}
+	<br />
+	{{ content }}
+</template>
